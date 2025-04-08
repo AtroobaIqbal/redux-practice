2ad1a5df89e4664ef0880e1de3c902b7a2152678
AtroobaIqbal <atroobaiqbal28@gmail.com>
2025-04-09 00:30:17 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskDetails from './components/TaskDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <TaskDetails />
+    </div>
+  );
+}
+
+export default App;
